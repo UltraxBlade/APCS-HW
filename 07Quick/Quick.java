db2760669e99aa17bfccf9497b37870347001fc3
UltraxBlade <mjrabinowitz01@gmail.com>
2017-03-23 15:17:44 -0400

    Dutch flag partition fixed

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,14 +1,13 @@
-
 public class Quick {
 	public static void quickSort(int[] arr){
 		quickSorter(arr, 0, arr.length);
 	}
 	
 	public static void quickSorter(int[] arr, int s, int e){
-		if (s<e){
-			int x = partition(arr, s, e);
-			quickSorter(arr, s, x);
-			quickSorter(arr, x+1, e);
+		if (s<e-1){
+			int[] x = partition(arr, s, e);
+			quickSorter(arr, s, x[0]);
+			quickSorter(arr, x[1]+1, e);
 		}
 	}
 	
@@ -16,53 +15,47 @@ public class Quick {
 		k--;
 		int s = 0;
 		int e = arr.length;
-		int x=partition(arr, s, e);
-		while(x!=k){
-			if (k<x){
-				e=x;
+		int[] x=partition(arr, s, e);
+		while((x[0]>k || x[1]<k) && s<e-1){
+			if (k<x[0]){
+				e=x[0];
 			} else {
-				s=x+1;
+				s=x[1]+1;
 			}
 			x=partition(arr, s, e);
 		}
 		return arr[k];
 	}
 	
-	private static int partition(int[] arr, int start, int end){
+	private static int[] partition(int[] arr, int start, int end){
 		int part=(int)(Math.random()*(end-start))+start;
 		int temp = arr[end-1];
 		arr[end-1]=arr[part];
 		arr[part]=temp;
-		int current=start;
-		for (int i = start; i<end-1; i++){
-			if(arr[i]<arr[end-1]){
-				temp=arr[current];
-				arr[current]=arr[i];
+		int less=start;
+		int greater=end-2;
+		int i = start;
+		while (i<=greater){
+			if (arr[i]==arr[end-1]){
+				i++;
+			} else if(arr[i]<arr[end-1]){
+				temp=arr[less];
+				arr[less]=arr[i];
 				arr[i]=temp;
-				current++;
+				less++;
+				i++;
+			} else {
+				temp=arr[greater];
+				arr[greater]=arr[i];
+				arr[i]=temp;
+				greater--;
 			}
 		}
-		temp=arr[current];
-		arr[current]=arr[end-1];
+		greater++;
+		temp=arr[greater];
+		arr[greater]=arr[end-1];
 		arr[end-1]=temp;
-		return current;
-	}
-	
-	public static void main(String[] args){
-		int[] L = new int[10];
-		int[] arr = new int[10];
-		for (int i = 0; i<L.length; i++){
-			L[i]=(int)(Math.random()*201)-100;
-			arr[i]=L[i];
-			System.out.print(L[i]);
-			System.out.print(" ");
-		}
-		System.out.println();
-		System.out.println(quickSelect(L, 4));
-		quickSort(arr);
-		for (int i = 0; i<arr.length; i++){
-			System.out.print(arr[i]);
-			System.out.print(" ");
-		}
+		int[] result = {less, greater};
+		return result;
 	}
 }
